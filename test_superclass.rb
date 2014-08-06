4fb895abef7e1508e99cd8b706407ccbd5535c24
Magdalena <madziaf86@tlen.pl>
2014-08-06 13:15:13 +0200

    add comments, change names to be more consistent

--- test_superclass.rb
+++ test_superclass.rb
@@ -4,14 +4,14 @@ class TestSuperclass
     symbols = self.methods
     strings = symbols.map(&:to_s)
     all_testmethods = strings.select { |m| m.include? "_test"}
-    all_testmethods.each do |methods|
-      self.send(methods)
+    all_testmethods.each do |test_method|
+      self.send(test_method) #send grabs a string and runs it as a method
     end
   end
 
+  # raises an exception if expected is not actual
+  # returns otherwise
   def assert(expected, actual, msg = "")
-    # raises an exception if expected is not actual
-    # returns otherwise
     if expected.eql? actual
       puts "victory"
     else
@@ -19,20 +19,21 @@ class TestSuperclass
     end
   end
 
+  # collect all inherited classes to run them later
+  # inherited finds out classes that inherit from superclass
   def self.inherited(subclass)
     @tests ||= []
     @tests.push(subclass)
     puts @tests.to_s
     puts "New subclass: #{subclass}"
-    # collect all inherited classes to run them later
   end
 
+  # run all the tests of all the classes that inherit from superclass
   def self.run_all_tests
-   # run all of them
-     puts @tests.to_s
+    puts @tests.to_s
     @tests.each do |test_class|
-    t = test_class.new
-    t.run
+      t = test_class.new
+      t.run
     end
   end
 end
